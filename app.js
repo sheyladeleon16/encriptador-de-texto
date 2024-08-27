b24486679f451cdae933bd98a3e89135dd7faa4e
sheyladeleon16 <sheyladeleon16@gmail.com>
2024-08-26 22:38:44 -0400

    actualizando estructura

---
+++ app.js
@@ -0,0 +1,18 @@
+const llaves = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"],  
+];
+
+function encriptaMensaje(mensaje) {
+    let mensajeEncriptado = "";
+    for(let i = 0; i < mensaje.length; i++){
+        let letra = mensaje[i];
+        let encriptada = letra;
+        for(let j = 0; j < llaves.length; j++){
+            if (letra == llaves )
+        }
+    }
+}
